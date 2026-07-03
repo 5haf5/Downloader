6ee7db9f413a716cb0f5d27717ea2dcd8876cb72
Hafik Tech <matejpesek2012@gmail.com>
2026-07-03 07:44:22 +0200

    Add Downloader class header file

---
+++ Downloader.h
@@ -0,0 +1,35 @@
+#ifndef DOWNLOADER_H
+#define DOWNLOADER_H
+
+#include <QMainWindow>
+#include <QProcess>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Downloader; }
+QT_END_NAMESPACE
+
+class Downloader : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Downloader(QWidget* parent = nullptr);
+    ~Downloader();
+
+private slots:
+    void on_selectFolderButton_clicked();
+    void on_downloadButton_clicked();
+    void on_btnUpdateYtDlp_clicked();
+
+private:
+    Ui::Downloader* ui;
+
+    QString selectedFolder;
+    QProcess* process;
+    QComboBox* codecBox;
+    QProcess* ffmpeg;
+
+    QString ytDlpPath = "C:/Tools/yt-dlp/yt-dlp.exe";
+};
+
+#endif // DOWNLOADER_H
